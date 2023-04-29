83959037fe9fd4f87604dafa707cb0debf2efed9
ujuniordev <ujunior@outlook.com>
2023-04-29 09:15:21 +0000

    Add image_filter choice field and image validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,7 +8,20 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
-    def validate_image()
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
