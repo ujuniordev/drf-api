f1d7a19cd1bc4c505ed3d364dd962c7e584e3d7e
ujuniordev <ujunior@outlook.com>
2023-04-30 19:01:09 +0000

    Fix env variables and allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://drf-api-jr.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'drf-api-jr.herokuapp.com/']
 
 
 # Application definition
