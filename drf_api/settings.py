191b9a84e7d9aefbfd45489e5fa3b0ff9a432e3a
ujuniordev <ujunior@outlook.com>
2023-04-30 19:10:10 +0000

    Fix env variables and allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -55,12 +55,12 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'drf-api-jr.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'drf-api-jr.herokuapp.com']
 
 
 # Application definition
